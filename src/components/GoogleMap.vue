b729be303a29acb45c5607c4676c2bd14abf4444
Izunyaaan <kanebluerivers@gmail.com>
2022-03-11 23:38:08 +0800

    done about us and a little tweaking of previous stuff

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <div class="d-flex justify-center">
-      <v-btn dark @click="respond" class="ma-5"
+      <v-btn dark @click="respond" class="ma-5 align-self-center"
         >Search for an authorized dealer near you.</v-btn
+      >
+      <v-btn dark to="/contact" class="ma-5 align-self-center"
+        >Interested in being a dealer? Contact us!</v-btn
       >
     </div>
     <br />
@@ -24,7 +27,7 @@ export default {
   methods: {
     respond: () =>
       alert(
-        "Unable to determine a dealer near you. You can try contacting us or ordering from us directly if you believe you live more than 50 miles from an authorized dealer. Sorry for the inconvenience."
+        "W are experiencing technical issues and are unable to determine a dealer near you. You can try contacting us or ordering from us directly if you believe you live more than 50 miles from an authorized dealer. Sorry for the inconvenience."
       ),
     geolocate: function () {
       navigator.geolocation.getCurrentPosition((position) => {
